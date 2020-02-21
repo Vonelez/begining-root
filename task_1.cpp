04e00b590663adc089929b8208bab9094af5bd79
sonya-bulanova <alice.dearago@gmail.com>
2020-02-21 21:16:51 +0300

    new

--- task_1.cpp
+++ task_1.cpp
@@ -2,21 +2,66 @@
 #include <TH1.h>
 #include <TFile.h>
 #include <TRandom.h>
+#include <TCanvas.h>
+#include <cstdio>
+#include <TObject.h>
 
 using namespace std;
 
-void task_1() {
-	TH1F *hist = new TH1F("hgaus", "A Gauss Function", 100, -5., 5.);
-	TRandom rnd;
-	Double_t x = 0;
+void task_1_new() {
+TCanvas *c1 = new TCanvas ("c1", "Three hists", 1200, 1200);
+c1->SetGrid();
+c1 -> cd();
 
-	for (int i = 0; i < 10000; ++i) {
-		x = rnd.Gaus(1.5, 1.0);
-		hist->Fill(x);
-		x = 0;
-	}
+TH1S *hist = new TH1S("hist", "Poisson distribution", 100, -5., 15.);
+TH1S *h2 = new TH1S ("h2", "Distributions; X; Y", 100, -5., 15.);
+TH1S *h3 = new TH1S ("h3", "Landau distribution; X; Y", 400, -5., 15.);
 
-	TFile outfile ("output.root", "RECREATE");
-	hist->Write("Gauss");
-	outfile.Close();
+ hist->SetMarkerStyle(16);
+   hist->SetFillColor(16);
+   h2->SetFillColor(42);
+   h3->SetFillColor(96);
+   TSlider *slider = 0;
+
+gRandom->SetSeed();
+const Int_t kUPDATE = 500;
+   Float_t xhist, xh2, xh3;
+   for ( Int_t i=0; i<10000; i++) {
+      xh2   = gRandom->Poisson(5);
+      xhist = gRandom->Gaus(4,1.5);
+
+int binmaxhist = hist -> GetMaximumBin();
+      double histx = hist -> GetXaxis()->GetBinCenter(binmaxhist);
+
+
+      xh3   = gRandom->Landau(1,0.15);
+      hist-> Fill(xhist);
+      h2-> Fill(xh2);
+      h3-> Fill(xh3); 
+      if (i && (i%kUPDATE) == 0) {
+         if (i == kUPDATE) {
+            h2->Draw();
+            hist->Draw("same");
+            h3->Draw("same");
+            c1->Update();}
+}
+}
+int maxix = hist -> GetMaximumBin();
+double maxx = hist -> GetXaxis() -> GetBinCenter(maxix);
+int maxiy = h2 -> GetMaximumBin();
+double maxy = h2 -> GetXaxis() -> GetBinCenter(maxiy);
+int maxiz = h3 -> GetMaximumBin();
+double maxz = h3 -> GetXaxis() -> GetBinCenter(maxiz);
+
+cout << "Gauss maximum: " << hist -> GetMaximum() << endl;
+cout << "it's abscissa: " << maxx << endl;
+cout << "Poisson maximum: " << h2 -> GetMaximum() << endl;
+cout << "it's abscissa: " << maxy << endl;
+cout << "Landau maximum: " <<  h3 -> GetMaximum() << endl;
+cout << "it's abscissa: " << maxz << endl;
+
+c1 -> Modified();
+c1 -> Print("outfile.pdf");
+hist->Write("threesome");
+
 }
