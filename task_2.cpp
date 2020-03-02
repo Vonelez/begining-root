09db65f8e81d771a823823522041165cafb7aa4c
sonya-bulanova <60941559+sonya-bulanova@users.noreply.github.com>
2020-03-02 21:38:52 +0300

    Add files via upload

--- task_2.cpp
+++ task_2.cpp
@@ -12,14 +12,14 @@ void task_2() {
 	Double_t aParam, bParam;
 	Double_t a_min = -0.27778;
 	Double_t a_max = 0.27778;
-	Double_t b_min = -50.0;
-	Double_t b_max = 50.0;
-	Double_t X = 0;
+	Double_t b_min = -1;
+	Double_t b_max = 1;
+	Double_t X;
 	Float_t Y = 0.0;
 	Float_t Y_det = 0.0;
 	TFile  *hfile = new TFile ("tree.root", "RECREATE");
 	TTree tree("T","Coordinates");
-	tree.Branch("Y_coord", &Y, "Y/F");
+	tree.Branch("X_coord", &X, "X/D");
 	tree.Branch("Y_detector", &Y_det, "Y_det/F");
 	TRandom rnd;
 	for (int i = 0; i < nEvents; ++i) {
@@ -27,13 +27,11 @@ void task_2() {
 		bParam = rnd.Uniform(b_min, b_max);
 		for (int k = 0; k<5; ++k){
 		X = stations[k];
+		tree.GetBranch("X_coord") -> Fill ();
 		Y = aParam * X + bParam;
-		if (Y < 50 && Y > -50){
-	Y_det = gRandom ->  Gaus(Y, 0.0150);
-	tree.SetEntries(4000);
-	tree.GetBranch("Y_coord") -> Fill ();
-	tree.GetBranch("Y_detector") -> Fill ();
-}
+		Y_det = gRandom ->  Gaus(Y, 0.0150);
+		tree.SetEntries(4000);
+		tree.GetBranch("Y_detector") -> Fill ();
 }
 }		
 	hfile -> Write();
