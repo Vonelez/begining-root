f2ea029fd71331b1dc31e17d9f356d7406ac7e20
sonya-bulanova <60941559+sonya-bulanova@users.noreply.github.com>
2020-03-07 23:12:01 +0300

    Add files via upload

--- task_2.cpp
+++ task_2.cpp
@@ -1,8 +1,4 @@
 #include <iostream>
-#include <TH1.h>
-#include <TFile.h>
-#include <TRandom.h>
-#include <TCanvas.h>
 #include <cstdio>
 #include <cmath>
 
@@ -15,22 +11,22 @@ void task_2() {
 	Double_t b_min = -1;
 	Double_t b_max = 1;
 	Double_t X;
-	Float_t Y = 0.0;
-	Float_t Y_det = 0.0;
+	Double_t Y[4];
+	Double_t Y_det[4];
 	TFile  *hfile = new TFile ("tree.root", "RECREATE");
 	TTree tree("T","Coordinates");
 	tree.Branch("X_coord", &X, "X/D");
-	tree.Branch("Y_detector", &Y_det, "Y_det/F");
+	tree.Branch("Y_detector", &Y_det[4], "Y_det[4]/D");
 	TRandom rnd;
+	tree.SetEntries(4000);	
 	for (int i = 0; i < nEvents; ++i) {
 		aParam = rnd.Uniform(a_min, a_max);
 		bParam = rnd.Uniform(b_min, b_max);
 		for (int k = 0; k<5; ++k){
 		X = stations[k];
 		tree.GetBranch("X_coord") -> Fill ();
-		Y = aParam * X + bParam;
-		Y_det = gRandom ->  Gaus(Y, 0.0150);
-		tree.SetEntries(4000);
+		Y[k] = aParam * X + bParam;
+		Y_det[k] = gRandom ->  Gaus(Y[k], 0.0150);
 		tree.GetBranch("Y_detector") -> Fill ();
 }
 }		
